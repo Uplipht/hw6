9c8fa57ff9d6f3293000636c7ed0504a951449ee
amandaalgebraic <amandajoee@gmail.com>
2018-10-04 11:10:46 -0400

    removed html

--- html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['flip',['flip',['../classpiece.html#ab898c5827a5859e4cddc9d61a814a873',1,'piece']]]
-];
--- html/search/functions_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['make_5fcomputer_5fmove',['make_computer_move',['../classmain__savitch__14_1_1game.html#afe47871075987c573b3e749f841a1782',1,'main_savitch_14::game']]],
-  ['make_5fhuman_5fmove',['make_human_move',['../classmain__savitch__14_1_1game.html#a1553dc75ccd05aa5cb6743a767eb4168',1,'main_savitch_14::game']]],
-  ['make_5fmove',['make_move',['../classmain__savitch__14_1_1_othello.html#a1066b280efa5cb41039585669282fe06',1,'main_savitch_14::Othello']]]
-];
